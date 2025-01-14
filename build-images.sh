8049113e8bff85cf475ffc8cedbf766f67713141
Matthew Rose <matthewaerose@gmail.com>
2025-01-14 11:11:45 -0600

    add 13.18

--- build-images.sh
+++ build-images.sh
@@ -4,5 +4,5 @@ set -eo pipefail
 
 docker context create cimg
 docker buildx create --use cimg
-docker buildx build --platform=linux/amd64,linux/arm64 --file 17.0/Dockerfile -t cimg/postgres:17.0 -t cimg/postgres:17.0 --push .
-docker buildx build --platform=linux/amd64,linux/arm64 --file 17.0/postgis/Dockerfile -t cimg/postgres:17.0-postgis -t cimg/postgres:17.0-postgis --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 13.18/Dockerfile -t cimg/postgres:13.18 -t cimg/postgres:13.18 --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 13.18/postgis/Dockerfile -t cimg/postgres:13.18-postgis -t cimg/postgres:13.18-postgis --push .
